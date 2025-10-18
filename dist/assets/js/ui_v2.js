09a79d3e2c4fe00a32925fc756f2356bb5dc0eef
kik_lenovo_17 <kik87828@gmail.com>
2025-10-18 20:40:44 +0900

    고객사 수정 완료

--- dist/assets/js/ui_v2.js
+++ dist/assets/js/ui_v2.js
@@ -46,3 +46,67 @@ function browserCheck() {
     document.querySelector("html").classList.add("kakao");
   }
 }
+
+/* dragg */
+
+function dragSwiper(selector) {
+  const tabWrap = document.querySelector(selector);
+  let touchstart = "ontouchstart" in window;
+
+  if (!tabWrap || touchstart) return; // 요소가 없으면 종료
+
+  const tabList = tabWrap.querySelector(".baguette_tab_list");
+  if (!tabList) return;
+
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+  let isDragging = false;
+
+  tabWrap.addEventListener("mousedown", (e) => {
+    if (window.innerWidth >= 1024) {
+      return;
+    }
+    isDown = true;
+    isDragging = false;
+    tabWrap.classList.add("active");
+    startX = e.pageX - tabWrap.offsetLeft;
+    scrollLeft = tabWrap.scrollLeft;
+  });
+
+  tabWrap.addEventListener("mouseleave", () => {
+    if (window.innerWidth >= 1024) {
+      return;
+    }
+    isDown = false;
+    tabWrap.classList.remove("active");
+  });
+
+  tabWrap.addEventListener("mouseup", () => {
+    if (window.innerWidth >= 1024) {
+      return;
+    }
+    isDown = false;
+    tabWrap.classList.remove("active");
+    setTimeout(() => (isDragging = false), 0);
+  });
+
+  tabWrap.addEventListener("mousemove", (e) => {
+    if (window.innerWidth >= 1024) {
+      return;
+    }
+    if (!isDown) return;
+    e.preventDefault();
+    isDragging = true;
+    const x = e.pageX - tabWrap.offsetLeft;
+    const walk = (x - startX) * 1;
+    tabWrap.scrollLeft = scrollLeft - walk;
+  });
+
+  // 탭 클릭 방지 (드래그 중일 때)
+  tabList.querySelectorAll(".baguette_tab").forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+      if (isDragging) e.preventDefault();
+    });
+  });
+}
